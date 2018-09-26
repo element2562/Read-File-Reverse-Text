34ec1703e2eae859d662b2a12236d4977e90a004
element2562 <element25668@gmail.com>
2018-09-25 20:50:57 -0500

    Finished exercise

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Users\Jacob\Workspace\dad's exercises\TextFile1.txt");
+            var splitText = text.Split(" ");
+            string newText = "";
+            for (int i = splitText.Length - 1; i > -1; i--)
+            {
+                newText += splitText[i] + " ";
+            }
+            System.IO.File.WriteAllText(@"C:\Users\Jacob\Workspace\dad's exercises\TextFile2.txt", newText);
+        }
+    }
+}
